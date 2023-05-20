e63cb82e328b92fe0f6a19071daa9d2c4fbdd4e8
Алиса Ярославцева <yaroslavtsevaalisa@yandex.ru>
2023-05-20 10:17:20 +0400

    fix

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,5 +10,6 @@ router.register(r'recipes', RecipeViewSet)
 router.register(r'tags', TagViewSet)
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
