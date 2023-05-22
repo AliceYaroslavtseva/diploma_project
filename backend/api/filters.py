2945999a7a392da0beb753b03fa7ef4fa8377e37
Алиса Ярославцева <yaroslavtsevaalisa@yandex.ru>
2023-05-22 08:32:44 +0400

    fix filter

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,19 +1,62 @@
+# from django_filters.rest_framework import FilterSet, filters
+# from recipes.models import Recipe
+
+
+# class RecipeFilter(FilterSet):
+
+#     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+#     author = filters.NumberFilter(field_name='author__id')
+#     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
+#     is_in_shopping_cart = filters.BooleanFilter(
+#         method='filter_is_in_shopping_cart'
+#     )
+
+#     class Meta:
+#         model = Recipe
+#         fields = ('tags', 'author')
+
+#     def filter_is_favorited(self, queryset, name, value):
+#         user = self.request.user
+#         if value and not user.is_anonymous:
+#             return queryset.filter(favorite__user=user)
+#         return queryset
+
+#     def filter_is_in_shopping_cart(self, queryset, name, value):
+#         user = self.request.user
+#         if value and not user.is_anonymous:
+#             return queryset.filter(shopping_cart__user=user)
+#         return queryset
+
+
+from django.contrib.auth import get_user_model
 from django_filters.rest_framework import FilterSet, filters
-from recipes.models import Recipe
+from recipes.models import Ingredient, Recipe, Tag
+
+User = get_user_model()
+
+
+class IngredientFilter(FilterSet):
+    name = filters.CharFilter(lookup_expr='startswith')
+
+    class Meta:
+        model = Ingredient
+        fields = ['name']
 
 
 class RecipeFilter(FilterSet):
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        to_field_name='slug',
+        queryset=Tag.objects.all(),
+    )
 
-    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    author = filters.NumberFilter(field_name='author__id')
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
-        method='filter_is_in_shopping_cart'
-    )
+        method='filter_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author')
+        fields = ('tags', 'author',)
 
     def filter_is_favorited(self, queryset, name, value):
         user = self.request.user
