4d3a7a385bde641ac2438c4cd2b99c4ebedfa8b0
Алиса Ярославцева <yaroslavtsevaalisa@yandex.ru>
2023-05-22 08:52:40 +0400

    fix

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -43,7 +43,6 @@ class IngredientFilter(FilterSet):
         fields = ('name',)
 
 
-
 class RecipeFilter(FilterSet):
     tags = filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
